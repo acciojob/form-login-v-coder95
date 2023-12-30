1ab01d75596b3fc4ae29bbe730b44192e454608e
poonam kushwaha <134260040+v-coder95@users.noreply.github.com>
2023-12-30 20:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const fname= document.getElementById("fname");
+const lname = document.getElementById("lname");
 function getFormvalue() {
-    //Write your code here
+     let str = fname.value+lname.value;
+	alert(str);
+	
+	//Write your code here
 
 }
